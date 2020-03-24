7adf6a45e5bf85b6e5e73d16f4ef5e6d41e61425
Srezzx <57859035+Srezzx@users.noreply.github.com>
2020-03-24 19:57:34 +0530

    Added better animations

--- form.js
+++ form.js
@@ -4,6 +4,8 @@ var option = document.getElementById("option");
 	$('#option').on("click",function(){
 	if(document.querySelector("#option").value == "Log In")
 	{
+		$(".topbar").slideUp(250);	
+		$(".topbar").slideDown(250);	
 		$(".phonenumber").fadeOut(250);
 		document.querySelector(".heading").textContent = "Log In";
  		document.querySelector(".textbelowheading").innerHTML= "Log In with your Credentials";
@@ -11,12 +13,12 @@ var option = document.getElementById("option");
 	}
 	else if(document.querySelector("#option").value == "Sign Up")
 	{
+
+		$(".topbar").slideUp(250);	
+		$(".topbar").slideDown(250);
 		$(".phonenumber").fadeIn(1000);
 		document.querySelector(".heading").textContent = "Sign Up";
  		document.querySelector(".textbelowheading").innerHTML= "Sign Up with your Credentials";
 		document.querySelector("#option").value = "Log In";
 	}
-	$(".colorofloginbar").fadeOut(250);
-	$(".colorofloginbar").fadeIn(250);
- 
 });
