591d71c26d72f8bd72f65e1d8fa0f4532543f560
Srezzx <57859035+Srezzx@users.noreply.github.com>
2020-03-25 11:43:50 +0530

    Animation changes

--- form.js
+++ form.js
@@ -4,21 +4,26 @@ var option = document.getElementById("option");
 	$('#option').on("click",function(){
 	if(document.querySelector("#option").value == "Log In")
 	{
-		$(".topbar").slideUp(250);	
-		$(".topbar").slideDown(250);	
-		$(".phonenumber").fadeOut(250);
-		document.querySelector(".heading").textContent = "Log In";
- 		document.querySelector(".textbelowheading").innerHTML= "Log In with your Credentials";
-		document.querySelector("#option").value = "Sign Up";
+		$(".topbar").fadeTo(125,0.5);	
+		$(".topbar").fadeTo(125,1.0);	
+		$(".phonenumber").fadeOut(250,function(){
+			document.querySelector(".heading").textContent = "Log In";
+ 			document.querySelector(".textbelowheading").innerHTML= "Log In with your Credentials";
+			document.querySelector("#option").value = "Sign Up";
+		});
+		
 	}
 	else if(document.querySelector("#option").value == "Sign Up")
 	{
 
-		$(".topbar").slideUp(250);	
-		$(".topbar").slideDown(250);
-		$(".phonenumber").fadeIn(1000);
+		$(".topbar").fadeTo(125,0.5);	
+		$(".topbar").fadeTo(125,1.0);	
+		$(".phonenumber").fadeIn(1000,function(){
+			
+		});
 		document.querySelector(".heading").textContent = "Sign Up";
  		document.querySelector(".textbelowheading").innerHTML= "Sign Up with your Credentials";
 		document.querySelector("#option").value = "Log In";
+	
 	}
 });
